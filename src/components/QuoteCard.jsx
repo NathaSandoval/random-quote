e5a5f78270a112e52925b1d8556f90c86e71b5da
Nathalia sandoval <nathaliasandovalnino@gmail.com>
2023-09-27 01:51:47 +0200

    initial commit

---
+++ src/components/QuoteCard.jsx
@@ -0,0 +1,26 @@
+import "./quoteCard.css"
+
+const QuoteCard = ({quote,handleChangeQuote }) => {
+    
+    
+  return (
+   <section className="quoteCard">
+
+    <article className="quoteCard__container">
+        <h1 className="quoteCard__title">Infogalax</h1>
+
+       
+        <p className="quoteCard__phrase">{quote.phrase}</p>
+
+        <button className="quoteCard__btn" onClick={handleChangeQuote}>Change</button>
+
+    </article>
+    
+    <footer className="quoteCard__footer">
+        Author: {quote.author}
+    </footer>
+
+   </section> 
+  )
+  }
+export default QuoteCard
---
+++ src/utils/random.js
@@ -0,0 +1,8 @@
+const getRandomElement = (elements) => {
+    const randonIndex = Math.floor(Math.random() * elements.length)
+    return elements[randonIndex]
+      }
+
+      export {
+        getRandomElement
+      }
